87fdfd9e55c5624020b0a796f4e4a5e9d04d6a30
the 6u6e <the6u6es@gmail.com>
2022-05-24 13:01:06 +0200

    .

--- conf.py
+++ conf.py
@@ -32,10 +32,10 @@ import sphinx_rtd_theme
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = ['sphinx.ext.todo',
-    'sphinx.ext.ifconfig',
-    'sphinxcontrib.fulltoc',
-    'sphinx_fontawesome']
+#extensions = ['sphinx.ext.todo',
+#   'sphinx.ext.ifconfig',
+#    'sphinxcontrib.fulltoc',
+#   'sphinx_fontawesome']
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
